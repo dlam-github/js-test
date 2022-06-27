c8d42e11d97cefae01037d9074bc359e7cf465df
Denis Lam <denis@bizpay.com>
2022-06-28 08:06:46 +1000

    Catch errors

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,17 +1,21 @@
 const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  return Promise.all(urls.map((url) => httpGet(url))).then((responses) =>
-    responses.map(({ body, status }) => {
-      const { message } = JSON.parse(body);
+  return Promise.all(urls.map((url) => httpGet(url)))
+    .then((responses) =>
+      responses.map(({ body, status }) => {
+        const { message } = JSON.parse(body);
 
-      const key = status === 200 ? "Arnie Quote" : "FAILURE";
+        const key = status === 200 ? "Arnie Quote" : "FAILURE";
 
-      return {
-        [key]: message,
-      };
-    })
-  );
+        return {
+          [key]: message,
+        };
+      })
+    )
+    .catch((error) => {
+      throw error;
+    });
 };
 
 module.exports = {
