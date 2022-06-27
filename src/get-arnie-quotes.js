d37cf4474a2f3beae1bca0cb6c99142c61637f62
Denis Lam <denis@bizpay.com>
2022-06-28 07:30:31 +1000

    Implemented getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,17 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(urls.map((url) => httpGet(url))).then((responses) =>
+    responses.map(({ body, status }) => {
+      const { message } = JSON.parse(body);
+
+      const key = status === 200 ? "Arnie Quote" : "FAILURE";
+
+      return {
+        [key]: message,
+      };
+    })
+  );
 };
 
 module.exports = {
